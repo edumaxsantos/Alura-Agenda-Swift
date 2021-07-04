8f20a8e1da5c0a5a9933d949724a61a208d3bfd4
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-07-04 01:00:35 -0300

    Implementado autenticação para apagar aluno

---
+++ Agenda/Componentes/AutenticacaoLocal.swift
@@ -0,0 +1,25 @@
+//
+//  AutenticacaoLocal.swift
+//  Agenda
+//
+//  Created by Eduardo Santos on 04/07/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class AutenticacaoLocal: NSObject {
+    
+    var error: NSError?
+
+    func autorizaUsuario(completion: @escaping (_ autenticado: Bool) -> Void) {
+        let contexto = LAContext()
+        
+        if contexto.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            contexto.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "autenticação necessária para apagar aluno") { resposta, erro in
+                completion(resposta)
+            }
+        }
+    }
+}
